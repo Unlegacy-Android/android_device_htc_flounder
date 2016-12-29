2aa7600e125e9bc87e2c3b4645f01f59e9a66eba
André Pinela <sheffzor@gmail.com>
2016-12-29 03:29:37 +0000

    Reorganize flounder tree
    Change-Id: I1d8fa1e24a1a34f3dbb38e65e6a98c9b081506b6
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- device.mk
+++ device.mk
@@ -14,68 +14,32 @@
 # limitations under the License.
 #
 
-PRODUCT_PACKAGES := \
-    libwpa_client \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf
+# Specific for 64 bit product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-ifeq ($(USE_SVELTE_KERNEL), true)
-LOCAL_KERNEL := device/htc/flounder_svelte-kernel/Image.gz-dtb
-else
-LOCAL_KERNEL := device/htc/flounder-kernel/Image.gz-dtb
-endif # USE_SVELTE_KERNEL
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+PRODUCT_AAPT_CONFIG := normal large xlarge
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
-# This ensures the needed build tools are available.
-# TODO: make non-linux builds happy with external/f2fs-tool; system/extras/f2fs_utils
-ifeq ($(HOST_OS),linux)
-TARGET_USERIMAGES_USE_F2FS := true
-endif
+# Dalvik VM config
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
-LOCAL_FSTAB := $(LOCAL_PATH)/fstab.flounder
-
-
-TARGET_RECOVERY_FSTAB = $(LOCAL_FSTAB)
-
-PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/init.flounder.rc:root/init.flounder.rc \
-    $(LOCAL_PATH)/init.flounder.usb.rc:root/init.flounder.usb.rc \
-    $(LOCAL_PATH)/init.recovery.flounder.rc:root/init.recovery.flounder.rc \
-    $(LOCAL_FSTAB):root/fstab.flounder \
-    $(LOCAL_PATH)/ueventd.flounder.rc:root/ueventd.flounder.rc
-
-# Copy flounder files as flounder64 so that ${ro.hardware} can find them
+# Init Files
+# Copy both flounder/flounder64 files so that ${ro.hardware} can find them
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.flounder.rc:root/init.flounder64.rc \
-    $(LOCAL_PATH)/init.flounder.usb.rc:root/init.flounder64.usb.rc \
-    $(LOCAL_FSTAB):root/fstab.flounder64 \
-    $(LOCAL_PATH)/init.recovery.flounder.rc:root/init.recovery.flounder64.rc \
-    $(LOCAL_PATH)/ueventd.flounder.rc:root/ueventd.flounder64.rc
+    $(LOCAL_PATH)/rootdir/init.flounder.rc:root/init.flounder.rc \
+    $(LOCAL_PATH)/rootdir/init.flounder.usb.rc:root/init.flounder.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.flounder.rc:root/init.recovery.flounder.rc \
+    $(LOCAL_PATH)/rootdir/fstab.flounder:root/fstab.flounder \
+    $(LOCAL_PATH)/rootdir/ueventd.flounder.rc:root/ueventd.flounder.rc \
+    $(LOCAL_PATH)/rootdir/init.flounder.rc:root/init.flounder64.rc \
+    $(LOCAL_PATH)/rootdir/init.flounder.usb.rc:root/init.flounder64.usb.rc \
+    $(LOCAL_PATH)/rootdir/fstab.flounder:root/fstab.flounder64 \
+    $(LOCAL_PATH)/rootdir/init.recovery.flounder.rc:root/init.recovery.flounder64.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.flounder.rc:root/ueventd.flounder64.rc
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/touch/touch_fusion.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_fusion.cfg \
-    $(LOCAL_PATH)/touch/maxim_fp35.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/maxim_fp35.bin \
-    $(LOCAL_PATH)/touch/touch_fusion.idc:system/usr/idc/touch_fusion.idc \
-    $(LOCAL_PATH)/touch/touch_fusion:$(TARGET_COPY_OUT_VENDOR)/bin/touch_fusion \
-    $(LOCAL_PATH)/touch/synaptics_dsx.idc:system/usr/idc/synaptics_dsx.idc
-
-# headset keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl
-
-PRODUCT_PACKAGES += \
-    libwpa_client \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf
-
+# These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
@@ -88,6 +52,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
@@ -104,40 +70,81 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
-    $(LOCAL_PATH)/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml
+    $(LOCAL_PATH)/rootdir/etc/permissions/com.nvidia.nvsi.xml:system/etc/permissions/com.nvidia.nvsi.xml
 
+# Audio Packages
+PRODUCT_PACKAGES += \
+    audio.primary.flounder \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libhtcacoustic \
+    libnvvisualizer \
+    sound_trigger.primary.flounder
+
+# Audio Files
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+    $(LOCAL_PATH)/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml \
+    $(LOCAL_PATH)/audio/nvaudio_conf.xml:system/etc/nvaudio_conf.xml
 
+# Audio Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.monitorRotation=true
+# Reduce client buffer size for fast audio output tracks
+# and configure audio low latency for 128 frames per buffer
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
+    audio_hal.period_size=128
+
+# Wi-Fi Pacages
+PRODUCT_PACKAGES += \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Wi-Fi Files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/enctune.conf:system/etc/enctune.conf
+    $(LOCAL_PATH)/bcmdhd/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(LOCAL_PATH)/bcmdhd/bcmdhd_lte.cal:system/etc/wifi/bcmdhd_lte.cal
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nvaudio_conf.xml:system/etc/nvaudio_conf.xml \
-    $(LOCAL_PATH)/nvcamera.conf:system/etc/nvcamera.conf \
-    $(LOCAL_PATH)/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    $(LOCAL_PATH)/bcmdhd_lte.cal:system/etc/wifi/bcmdhd_lte.cal
+# Wi-Fi Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
 
+# Wi-Fi Dependencies
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
+
+# Bluetooth Files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bcm4350b1.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4350b1.hcd \
     $(LOCAL_PATH)/bluetooth/BCM4354_003.001.012.0319.0690_ORC.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4350c0.hcd \
     $(LOCAL_PATH)/bluetooth/bcm4354.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4354.hcd \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/bt_vendor.conf
 
+# Bluetooth Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/sys/module/flounder_bdaddress/parameters/bdaddress
+
+# NFC Packages
+PRODUCT_PACKAGES += \
+    nfc_nci.bcm2079x.default \
+    NfcNci \
+    Tag
+
+# NFC Files
+PRODUCT_COPY_FILES += \
+    device/htc/flounder/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/htc/flounder/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+
+# GPS Files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/bcm/gps.bcm47521.conf:system/etc/gps.bcm47521.conf \
     $(LOCAL_PATH)/gps/bcm/glgps:system/bin/glgps \
     $(LOCAL_PATH)/gps/bcm/gpsconfig.xml:system/etc/gpsconfig.xml \
-    $(LOCAL_PATH)/gps/bcm/lib64/gps.bcm47521.so:system/lib64/hw/gps.bcm47521.so
-
-# GPS: QCT MODULES
-PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/bcm/lib64/gps.bcm47521.so:system/lib64/hw/gps.bcm47521.so\
     $(LOCAL_PATH)/gps/qct/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/qct/SuplRootCert:system/etc/SuplRootCert \
     $(LOCAL_PATH)/gps/qct/lib/libgeofence.so:system/lib/libgeofence.so \
@@ -159,51 +166,62 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/qct/lib64_vendor/libmdmdetect.so:vendor/lib64/libmdmdetect.so \
     $(LOCAL_PATH)/gps/qct/lib64_vendor/libperipheral_client.so:vendor/lib64/libperipheral_client.so
 
-
-# NFC feature + config files
+# Touchscreen Files
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    device/htc/flounder/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/htc/flounder/nfc/libnfc-brcm-20795a10.conf:system/etc/libnfc-brcm-20795a10.conf
+    $(LOCAL_PATH)/touch/touch_fusion.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_fusion.cfg \
+    $(LOCAL_PATH)/touch/maxim_fp35.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/maxim_fp35.bin \
+    $(LOCAL_PATH)/touch/touch_fusion.idc:system/usr/idc/touch_fusion.idc \
+    $(LOCAL_PATH)/touch/touch_fusion:$(TARGET_COPY_OUT_VENDOR)/bin/touch_fusion \
+    $(LOCAL_PATH)/touch/synaptics_dsx.idc:system/usr/idc/synaptics_dsx.idc
 
-PRODUCT_AAPT_CONFIG := normal large xlarge
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# Media Files
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media/enctune.conf:system/etc/enctune.conf \
+    $(LOCAL_PATH)/rootdir/etc/nvcamera.conf:system/etc/nvcamera.conf
 
-PRODUCT_CHARACTERISTICS := tablet,nosdcard
-
-ifneq ($(filter volantis volantisf, $(TARGET_PRODUCT)),)
-# Wifi-Only overlays.
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/wifi_only_overlay \
-    $(LOCAL_PATH)/overlay
-else
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
-endif
-
-# NFC packages
-PRODUCT_PACKAGES += \
-    nfc_nci.bcm2079x.default \
-    NfcNci \
-    Tag \
-
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
+# HALs Packages
 PRODUCT_PACKAGES += \
     power.flounder \
     lights.flounder \
     sensors.flounder \
     thermal.flounder
 
-# Filesystem management tools
+# Filesystem Packages
 PRODUCT_PACKAGES += \
-    fsck.f2fs mkfs.f2fs
+    fsck.f2fs \
+    mkfs.f2fs
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0 \
+# Charger Packages
+# for off charging mode
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+# Charger Properties
+# Allows healthd to boot directly from charger mode rather than initiating a reboot.
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.enable_boot_charger_mode=1
+
+# Misc
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory \
+    LiveWallpapersPicker \
+    Launcher3
+# Package for keyboard key mappings
+PRODUCT_PACKAGES += \
+    VolantisKeyboard
+# Package for launcher layout
+PRODUCT_PACKAGES += \
+    VolantisLayout
+
+# Misc Properties
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196609 \
     ro.sf.lcd_density=320 \
     ro.hwui.texture_cache_size=72 \
@@ -218,51 +236,19 @@ PRODUCT_PROPERTY_OVERRIDES := \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024 \
     ro.hwui.disable_scissor_opt=true \
-    ro.bt.bdaddr_path=/sys/module/flounder_bdaddress/parameters/bdaddress \
     ro.frp.pst=/dev/block/platform/sdhci-tegra.3/by-name/PST \
     ro.ril.def.agps.mode=1 \
     persist.tegra.compositor=glcomposer
 
-# setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
-
-# for off charging mode
-PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
-
-# for audio
-PRODUCT_PACKAGES += \
-    audio.primary.flounder \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    libhtcacoustic \
-    libnvvisualizer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.monitorRotation=true
-
-# for sound trigger on DSP
-PRODUCT_PACKAGES += \
-    sound_trigger.primary.flounder
-
-# for keyboard key mappings
-PRODUCT_PACKAGES += \
-	VolantisKeyboard
-
-# for launcher layout
-PRODUCT_PACKAGES += \
-    VolantisLayout
-
-# Allows healthd to boot directly from charger mode rather than initiating a reboot.
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.enable_boot_charger_mode=1
-
-# Reduce client buffer size for fast audio output tracks
-# and configure audio low latency for 128 frames per buffer
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1 \
-    audio_hal.period_size=128
+# Device Overlays
+ifneq ($(filter flounder, $(TARGET_PRODUCT)),)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/common \
+    $(LOCAL_PATH)/overlay/wifi
+else
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/common
+endif
 
 # In userdebug, add minidebug info the the boot image and the system server to support
 # diagnosing native crashes.
@@ -275,24 +261,34 @@ ifneq (,$(filter userdebug, $(TARGET_BUILD_VARIANT)))
     $(call add-product-dex-preopt-module-config,wifi-service,--generate-mini-debug-info)
 endif
 
-# add verity dependencies
+# Verity
+# Only include verity on user builds for CM
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_COPY_FILES += \
+    device/htc/flounder/fstab-verity.flounder:root/fstab.flounder \
+    device/htc/flounder/fstab-verity.flounder:root/fstab.flounder64
 $(call inherit-product, build/target/product/verity.mk)
 PRODUCT_SUPPORTS_BOOT_SIGNER := false
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/APP
 PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
 
-# for warning
+# Verity Packages
+# For warning
 PRODUCT_PACKAGES += \
     slideshow \
     verity_warning_images
+endif
 
-$(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
-$(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
-$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
-$(call inherit-product-if-exists, vendor/htc/flounder/audio/lifevibes/lvve/device-vendor-lvve.mk)
-$(call inherit-product-if-exists, vendor/htc/flounder/audio/tfa/device-vendor-tfa.mk)
-
+# Keystore Packages
 # Add dependency of the proprietary keystore.flounder module.
 PRODUCT_PACKAGES += \
     libkeymaster_messages
+
+# Open Source Vendor Code
+$(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
+
+# Vendor Blobs
+$(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/flounder/audio/lifevibes/lvve/device-vendor-lvve.mk)
+$(call inherit-product-if-exists, vendor/htc/flounder/audio/tfa/device-vendor-tfa.mk)
