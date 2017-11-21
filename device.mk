LineageOS/android_device_huawei_angler

Add drm HALs to device makefiles
bug:32815560
Change-Id: Ic2743eab4a8c0e253e74b1fad55bf7f173c0aab4

device.mk
@@ -193,6 +193,14 @@ PRODUCT_PACKAGES += \
     lights.angler \
     android.hardware.light@2.0-impl
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+
+#ifeq ($(ENABLE_TREBLE), true)
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-service
+#endif
+
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_PACKAGES += \
     audio.primary.msm8994 \

Unlegacy-Android/android_device_htc_flounder

Signed-off-by: André Pinela <sheffzor@gmail.com>

device.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
     lights.flounder \
     hwcomposer.flounder
 
+# DRM HAL
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Sensors HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \